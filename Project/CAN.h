be24e71e09302ab9eef997cae3676d6f22147807
methosmen <morten@gjedrem.net>
2015-07-03 23:22:57 +0200

    moved CAN messanges to CAN.h

--- Project/CAN.h
+++ Project/CAN.h
@@ -1,2 +1,50 @@
 void InitCAN(void);
 uint8_t CANTx(uint32_t address, uint8_t length, uint8_t data[8]);
+
+
+/* Macro list of all the Tx/Rx message ID's for this module
+-----------------------------------------------------------
+0x0 = unknown ID.
+*/
+
+
+/* Tx list 
+
+Below is some information about the contents of the messages.
+Example	: w1.1 = wheelsensor 1 part 1.
+				: w1.2 = wheelsensor 1 part 2.
+-------------------------------------
+ID			D1		D2		D3		D4		D5
+0x200		W1.1	w1.2	w2.1	w2.2	-
+0x40		-			-			-			-			-
+0x41		-			-			-			-			-
+0x600		-			-			-			-			-
+
+*/
+
+#define TX_IWD_TIMEOUT	0x600
+
+/*
+CAN transmit messages
+*/
+#define CAN_ECHO 0x30					//CAN echo message
+#define ECU_FAILED_ECHO 0x41 	//ECU did not respond	
+#define RPIU_FAILED_ECHO 0x42	//RPIU did not respond
+#define DU_FAILED_ECHO 0x43		//DU did not respond
+#define PU_FAILED_ECHO 0x44		//PU did not respond
+
+#define CAN_MSG_CURRENT_SENSOR 0x011	
+#define CAN_MSG_HV_SENSOR 0x014
+#define CAN_MSG_LV_SENSOR 0x015	
+#define CAN_MSG_POWER 0x016
+
+
+/*
+CAN recive messages
+*/
+#define ECU_KICKED_CAN 	0x31 		// Engine Control Unit
+#define RPIU_KICKED_CAN 0x32 		// RPI Unit
+#define DU_KICKED_CAN 	0x33		// Dashboard Unit
+#define PU_KICKED_CAN 	0x34		// Pedalbox Unit
+
+
